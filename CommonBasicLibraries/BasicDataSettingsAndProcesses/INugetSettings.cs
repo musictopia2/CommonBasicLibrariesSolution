8eb6249bf385f36c7cc47d7d6198a1d2c12fd4c3
Andy <musictopia2@hotmail.com>
2021-11-12 08:49:46 -0700

    Prepped For Nuget.  Plus minor update to interface for nugetsettings.

--- CommonBasicLibraries/BasicDataSettingsAndProcesses/INugetSettings.cs
+++ CommonBasicLibraries/BasicDataSettingsAndProcesses/INugetSettings.cs
@@ -2,12 +2,9 @@
 {
     public interface INugetSettings
     {
-        /// <summary>
-        /// Need to provide a list of the paths for the directories you are using with this system.  will be a function
-        /// </summary>
-        /// <returns></returns>
-        Task<BasicList<string>> GetProjectListsAsync();
-        Task<string> GetDataPathAsync(); //this is where you will store all the files needed for the system to work.
+        string OpenSourcePath(); //useful for monitoring processes.
+        //was going to remove this and do something else.  decided against it after all.  does mean the main data has to be a file.  needs file so it can monitor anyways.
+        string GetDataPath(); //this is where you will store all the files needed for the system to work.  decided no need to async this anyways now.
         Task<string> GetKeyAsync(); //you need to provide the key which is used to upload packages.
         //decided to make it clear its async.  the good thing about this interface is if i decided to get the keys via keyvault, should still work.
     }
