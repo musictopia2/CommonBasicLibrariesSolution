e2734f8ae143a940bceec336aa866c5f7d456bd1
Andy <musictopia2@hotmail.com>
2021-12-18 07:21:21 -0700

    New interface for fast enum. Its preparation for a new custom fast enum system.

---
+++ CommonBasicLibraries/BasicDataSettingsAndProcesses/IFastEnum.cs
@@ -0,0 +1,10 @@
+﻿namespace CommonBasicLibraries.BasicDataSettingsAndProcesses;
+public interface IFastEnum<A>
+    where A : struct, IFastEnum<A>
+{
+    int Value { get; }
+    string Name { get; }
+    string Words { get; } //i like the idea of words too.  this means i don't have to worry about performance problems when getting words.
+    BasicList<A> CompleteList { get; }
+    bool IsNull { get; }
+}
