88a9bd9fa9d8d4e7d36090e7b3213e281f4157a3
Andy <musictopia2@hotmail.com>
2021-12-20 07:43:18 -0700

    Added a second custom enum interface. Useful so if list is not needed, then can reference the other one instead.

---
+++ CommonBasicLibraries/BasicDataSettingsAndProcesses/IFastEnumList.cs
@@ -0,0 +1,6 @@
+﻿namespace CommonBasicLibraries.BasicDataSettingsAndProcesses;
+public interface IFastEnumList<A> : IFastEnumSimple
+    where A : struct, IFastEnumList<A>
+{
+    BasicList<A> CompleteList { get; }
+}
--- CommonBasicLibraries/BasicDataSettingsAndProcesses/IFastEnumSimple.cs
+++ CommonBasicLibraries/BasicDataSettingsAndProcesses/IFastEnumSimple.cs
@@ -1,10 +1,8 @@
 ﻿namespace CommonBasicLibraries.BasicDataSettingsAndProcesses;
-public interface IFastEnum<A>
-    where A : struct, IFastEnum<A>
+public interface IFastEnumSimple
 {
     int Value { get; }
     string Name { get; }
     string Words { get; } //i like the idea of words too.  this means i don't have to worry about performance problems when getting words.
-    BasicList<A> CompleteList { get; }
     bool IsNull { get; }
 }
