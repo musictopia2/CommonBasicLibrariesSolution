f728bcb034f849ce160963767033683c7f770fcb
Andy <musictopia2@hotmail.com>
2022-02-07 11:06:34 -0700

    Decided to put back in IViewModelBase So if that is used for mappings, then will do shallow copies of objects instead of deep copies.

---
+++ CommonBasicLibraries/BasicDataSettingsAndProcesses/IViewModelBase.cs
@@ -0,0 +1,4 @@
+﻿namespace CommonBasicLibraries.BasicDataSettingsAndProcesses;
+//this is needed so if its mappable but can't just serialize/deserialize because it has constructors, can still do mappings.
+//would do shallow copies of complex objects within these.
+public interface IViewModelBase { }
