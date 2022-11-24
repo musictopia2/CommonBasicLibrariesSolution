0dfb1631250c2c9d552201c01ba3ef012656b5ed
Andy Walter <musictopia2@hotmail.com>
2022-11-24 13:00:04 -0700

    Add ability to exit full screen.

--- CommonBasicLibraries/MediaHelpers/Helpers/MediaGlobalClass.cs
+++ CommonBasicLibraries/MediaHelpers/Helpers/MediaGlobalClass.cs
@@ -4,4 +4,5 @@ public static class MediaGlobalClass
     public static bool IsTesting { get; set; } //this is for media stuff but does not care about specific media stuff like movies, music, etc.
     public static Action? ProcessFullScreen { get; set; } //some processes needs to set/run.  like youtube needing to run if any.
     public static string StartProgress = Extensions.StartProgress();
+    public static Action? ExitFullScreen { get; set; } //even though libraries don't need this, would be nice to have a common place to put this to process.
 }
