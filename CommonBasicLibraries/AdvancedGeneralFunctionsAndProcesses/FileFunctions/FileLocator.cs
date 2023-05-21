b9d896eb74ea0caf7aae2633bb9f6ea8acbcc8fd
Andy Walter <musictopia2@hotmail.com>
2023-05-21 16:01:59 -0700

    Improved the file locator function. Changed it where if its directly inside where we want, then that becomes the starting point. Allows the possibility of copying from one version to another without having to change the names. Only catch is one thing has to be consistent between moving around.

--- CommonBasicLibraries/AdvancedGeneralFunctionsAndProcesses/FileFunctions/FileLocator.cs
+++ CommonBasicLibraries/AdvancedGeneralFunctionsAndProcesses/FileFunctions/FileLocator.cs
@@ -2,14 +2,92 @@
 public static class FileLocator
 {
     public static string MainLocation { get; set; } = "";
-    public static string GetLocation(string name)
+    private static string _lookup = "";
+    private static string _previousLocation = "";
+    private static string _originalPath = "";
+    private static string OldLocation(string name)
     {
         string olds = MainLocation;
-        string path = aa1.GetApplicationPath();
-        int finds = path.IndexOf(MainLocation);
-        string modified = path.Substring(0, finds);
+        _originalPath = aa1.GetApplicationPath();
+        int finds = _originalPath.IndexOf(MainLocation);
+        if (finds == -1)
+        {
+            return "";
+        }
+        string modified = _originalPath.Substring(0, finds);
         modified += olds += @"\";
         modified += name;
         return modified;
+    }
+    private static string StartingPath()
+    {
+        string path = "";
+        path = ff1.GetParentPath(_originalPath);
+        4.Times(() =>
+        {
+            path = ff1.GetParentPath(path);
+        });
+        return path;
+    }
+    private static void StartLookup()
+    {
+        if (_previousLocation == MainLocation)
+        {
+            return;
+        }
+        _lookup = MainLocation.ToLower();
+        _lookup = _lookup.Replace(@"\", "");
+        _lookup = _lookup.Replace(@"/", "");
+        _previousLocation = MainLocation;
+    }
+    private static string GetFolderPath(string starting)
+    {
+        string path = starting;
+        do
+        {
+            if (FolderExist(path))
+            {
+                return path;
+            }
+            if (path == @"C:\")
+            {
+                throw new CustomBasicException($"Path not found for getting location.  Location attempted was {MainLocation}");
+            }
+            path = ff1.GetParentPath(path);
+        } while (true);
+    }
+    private static bool FolderExist(string path)
+    {
+        BasicList<string> list = ff1.DirectoryList(path);
+        foreach (var item in list)
+        {
+            string name = ff1.FileName(item);
+
+            if (name.ToLower() == _lookup)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    private static string NewLocation(string name)
+    {
+        if (_originalPath == "")
+        {
+            _originalPath = aa1.GetApplicationPath();
+        }
+        string firsts = StartingPath();
+        string folders = GetFolderPath(firsts);
+        return $"{folders}/{name}";
+    }
+    public static string GetLocation(string name)
+    {
+        string originalProcesses = OldLocation(name);
+        if (originalProcesses == "")
+        {
+            StartLookup();
+            return NewLocation(name);
+        }
+        return originalProcesses;
     }
 }
