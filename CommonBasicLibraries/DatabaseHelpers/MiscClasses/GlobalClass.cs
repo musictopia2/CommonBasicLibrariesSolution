67e006f7db62a987acbadb7c05b8d25d0b0b9425
Andy Walter <musictopia2@hotmail.com>
2024-02-08 09:14:09 -0700

    Added new delegate for adding sqlite lite databases for document databases.

--- CommonBasicLibraries/DatabaseHelpers/MiscClasses/GlobalClass.cs
+++ CommonBasicLibraries/DatabaseHelpers/MiscClasses/GlobalClass.cs
@@ -5,4 +5,5 @@ public static class GlobalClass
     public static ISQLiteConnector? SQLiteConnector { get; set; }
     public static ISQLServerConnector? SQLServerConnector { get; set; }
     public static IMySQLConnector? MySQLConnector { get; set; }
+    public static Action<string>? CreateSqliteDatabase { get; set; }
 }
