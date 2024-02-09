8b9cec3841884187a0b5833be6cc6e7e5a6d8cd5
Andy Walter <musictopia2@hotmail.com>
2024-02-09 10:31:32 -0700

    Updated the interface so the order shows correct when needed.

--- CommonBasicLibraries/DatabaseHelpers/AbstractStaticInterfaces/ISqlDocumentConfiguration.cs
+++ CommonBasicLibraries/DatabaseHelpers/AbstractStaticInterfaces/ISqlDocumentConfiguration.cs
@@ -1,7 +1,8 @@
 ﻿namespace CommonBasicLibraries.DatabaseHelpers.AbstractStaticInterfaces;
 #if NET7_0_OR_GREATER
-public interface ISqlDocumentConfiguration : ISqlDatabaseConfiguration
+public interface ISqlDocumentConfiguration
 {
+    abstract static string DatabaseName { get; }
     abstract static string CollectionName { get; }
 }
 #endif
