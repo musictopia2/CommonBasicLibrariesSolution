32f988e756e176a016874214584a5e7aaa8c261e
Andy Walter <musictopia2@hotmail.com>
2024-04-04 16:04:54 -0700

    Decided to rename the joined entity interface. Also, decided that the values being sent in can be null as well.

--- CommonBasicLibraries/DatabaseHelpers/EntityInterfaces/IJoinedEntity.cs
+++ CommonBasicLibraries/DatabaseHelpers/EntityInterfaces/IJoinedEntity.cs
@@ -3,11 +3,11 @@
 public interface IJoinedEntity<D1> : ISimpleDatabaseEntity //hopefully i don't need more than 3 joined.  if so, rethink
     where D1: class, ISimpleDatabaseEntity
 {
-    void AddRelationships(D1 entity);
+    void AddRelationships(D1? entity);
 }
-public interface IJoinEntity<D1, D2> : ISimpleDatabaseEntity
+public interface IJoinedEntity<D1, D2> : ISimpleDatabaseEntity
     where D1: class, ISimpleDatabaseEntity
     where D2: class, ISimpleDatabaseEntity
 {
-    void AddRelationships(D1 firstEntity, D2 secondEntity);
+    void AddRelationships(D1? firstEntity, D2? secondEntity);
 }
