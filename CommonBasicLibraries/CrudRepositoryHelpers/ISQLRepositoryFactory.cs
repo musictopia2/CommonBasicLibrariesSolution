10b9f30518857002761d2c163d059e946ef845cd
Andy Walter <musictopia2@hotmail.com>
2024-12-07 18:41:05 -0700

    Removed factory since that did not work so well.

--- CommonBasicLibraries/CrudRepositoryHelpers/ISQLRepositoryFactory.cs
+++
@@ -1,8 +0,0 @@
-﻿using CommonBasicLibraries.DatabaseHelpers.SourceGeneratorHelpers; //not common enough
-namespace CommonBasicLibraries.CrudRepositoryHelpers;
-//i think in the general helpers library is fine.
-public interface ISQLRepositoryFactory
-{
-    object CreateSQLRepository<TEntity>(string connectionString, IDatabaseConnectionManager manager)
-        where TEntity : class, ISimpleDatabaseEntity, ICommandQuery<TEntity>, ITableMapper<TEntity>;
-}
