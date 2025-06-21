2768594aa7d6c7c7743383c02b64aa513c16ee4a
Andy Walter <musictopia2@hotmail.com>
2025-06-20 19:47:33 -0700

    Add DefaultFolderPath to CssToolConfiguration
    Introduced a new property `DefaultFolderPath` in the
    CssToolConfiguration class, initialized to "Resources".
    This serves as a default folder path when none is specified.

--- CommonBasicLibraries/AdvancedGeneralFunctionsAndProcesses/CssHelpers/CssToolConfiguration.cs
+++ CommonBasicLibraries/AdvancedGeneralFunctionsAndProcesses/CssHelpers/CssToolConfiguration.cs
@@ -2,6 +2,7 @@
 public class CssToolConfiguration
 {
     public string DefaultBasePath { get; set; } = "";
+    public string DefaultFolderPath { get; set; } = "Resources"; //default if nothing was specified.
     public BasicList<CssFileConfig> Files { get; set; } = [];
     public BasicList<string> GlobalExcludedClasses { get; set; } = [];
 }
