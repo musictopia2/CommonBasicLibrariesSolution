f7fd9a83d1f66821ea5d1f61eee7f4d2373ae061
Andy Walter <musictopia2@hotmail.com>
2025-07-04 13:56:05 -0700

    Add UploadFileInfo record for file upload management
    Introduced a new namespace `CommonBasicLibraries.AdvancedGeneralFunctionsAndProcesses.UploadHelpers` and created the `UploadFileInfo` record. This record includes properties for `PropertyName`, `MaxSize`, and `AllowedContentTypes`, facilitating structured management and validation of uploaded files.

---
+++ CommonBasicLibraries/AdvancedGeneralFunctionsAndProcesses/UploadHelpers/UploadFileInfo.cs
@@ -0,0 +1,4 @@
+﻿namespace CommonBasicLibraries.AdvancedGeneralFunctionsAndProcesses.UploadHelpers;
+public record UploadFileInfo(string PropertyName,
+        long MaxSize,
+        BasicList<string> AllowedContentTypes);
