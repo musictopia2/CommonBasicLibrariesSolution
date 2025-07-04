449203c26a2bf9b544b941539f7b6db3c3d9a02a
Andy Walter <musictopia2@hotmail.com>
2025-07-04 14:38:54 -0700

    Add UploadFileAttribute for file upload validation
    Introduced a new namespace `CommonBasicLibraries.AdvancedGeneralFunctionsAndProcesses.UploadHelpers` to organize upload-related code. Added the `UploadFileAttribute` class with an `IsRequired` property to indicate if a property is mandatory for file uploads.

---
+++ CommonBasicLibraries/AdvancedGeneralFunctionsAndProcesses/UploadHelpers/UploadFileAttribute.cs
@@ -0,0 +1,7 @@
+﻿namespace CommonBasicLibraries.AdvancedGeneralFunctionsAndProcesses.UploadHelpers;
+
+[AttributeUsage(AttributeTargets.Property)]
+public class UploadFileAttribute : Attribute
+{
+    public bool IsRequired { get; set; } = false;
+}
