631014934fc5321e2041e0a849bd1990ad4bc332
Andy Walter <musictopia2@hotmail.com>
2025-07-10 07:46:58 -0700

    Added new helper for linux.

---
+++ CommonBasicLibraries/AdvancedGeneralFunctionsAndProcesses/FileFunctions/PathHelper.cs
@@ -0,0 +1,21 @@
+﻿namespace CommonBasicLibraries.AdvancedGeneralFunctionsAndProcesses.FileFunctions;
+public static class LinuxPathHelper
+{
+    public static Dictionary<string, string> PathCaseMappings { get; set; } = new();
+    // Call this once to hook into your bb1.GetCleanedPath delegate
+    public static void SetupLinuxPathMapper()
+    {
+        bb1.GetCleanedPath = DefaultLinuxPathMapper;
+    }
+    private static string DefaultLinuxPathMapper(string path)
+    {
+        var output = path.Replace("\\", "/");
+
+        foreach (var kvp in PathCaseMappings)
+        {
+            output = output.Replace(kvp.Key, kvp.Value, StringComparison.OrdinalIgnoreCase);
+        }
+
+        return output;
+    }
+}
