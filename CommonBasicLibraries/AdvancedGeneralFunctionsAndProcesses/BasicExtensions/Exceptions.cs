862931229094583088c3f5b0b3bf8c9a57ccac23
Andy Walter <musictopia2@hotmail.com>
2025-11-19 13:56:49 -0700

    Added new extension to throw if null for my custombasicexception.   allowing the way to create new extensions for my exceptions.

---
+++ CommonBasicLibraries/AdvancedGeneralFunctionsAndProcesses/BasicExtensions/Exceptions.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics.CodeAnalysis;
+namespace CommonBasicLibraries.AdvancedGeneralFunctionsAndProcesses.BasicExtensions;
+public static class Exceptions
+{
+    extension(CustomBasicException)
+    {
+        public static void ThrowIfNull([NotNull] object? value)
+        {
+            if (value is null)
+            {
+                throw new CustomBasicException("Was null.  Not correct");
+            }
+        }
+        
+    }
+}
